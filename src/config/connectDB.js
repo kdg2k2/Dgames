dbb42e6fd8f79af30a0b71a82036ca16041db09e
DG <kdg2k2@gmail.com>
2023-05-29 15:36:02 +0700

    thử kết nối bằng mongo web

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+//mongodb://127.0.0.1/Dgames
 async function connect() {
 	try {
-		await mongoose.connect('mongodb://127.0.0.1/Dgames', {
+		await mongoose.connect('mongodb+srv://kdg2k2:bachDuong2k2@cluster0.4r083er.mongodb.net/Dgames', {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
